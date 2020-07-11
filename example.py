97abc424642d01a20b970a1372226c897d8778b0
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2020-07-11 16:14:50 -0500

    example2

--- example.py
+++ example.py
@@ -3,3 +3,15 @@ import math
 
 def example():
     pass
+
+
+def example_two():
+    pass
+
+
+def another_func():
+    pass
+
+
+def one_more():
+    pass
---
+++ exampletwo.py
@@ -0,0 +1,6 @@
+def function_one():
+    pass
+
+
+def function_two():
+    pass
